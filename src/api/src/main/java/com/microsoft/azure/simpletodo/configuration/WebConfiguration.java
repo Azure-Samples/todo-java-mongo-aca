3a870aee944306ba97c01387d5337357ed144156
azure-sdk <azuresdk@microsoft.com>
2023-04-04 07:54:59 +0000

    Synchronize repo from Repoman

--- src/api/src/main/java/com/microsoft/azure/simpletodo/configuration/WebConfiguration.java
+++ src/api/src/main/java/com/microsoft/azure/simpletodo/configuration/WebConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import java.io.*;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
@@ -24,9 +25,18 @@ public class WebConfiguration implements WebMvcConfigurer {
                 String apiUrl = System.getenv("REACT_APP_WEB_BASE_URL");
 
                 if (apiUrl != "") {
+                    String localHostUrl = "http://localhost:3000/";
                     registry.addMapping("/**").allowedOrigins("https://portal.azure.com",
-                            "https://ms.portal.azure.com", "http://localhost:3000/",
+                            "https://ms.portal.azure.com", localHostUrl,
                             apiUrl).allowedMethods("*").allowedHeaders("*");
+                    String fileName = Thread.currentThread().getStackTrace()[1].getFileName();
+                    File file = new File(fileName);
+                    String absolutePath = file.getAbsolutePath();
+                    System.out.println(
+                            "CORS with " + localHostUrl
+                                    + " is allowed for local host debugging. If you want to change pin number, go to "
+                                    + absolutePath);
+
                 } else {
                     registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
                     System.out.println(
